59ca678a160b5c9d2109ab54fb6474e3ef329242
Ardra66 <ardrazz68@gmail.com>
2026-09-11 13:02:24 +0530

    initialize portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[#050505] text-white">
+      <h1 className="text-4xl font-bold text-center pt-20">
+        Ardra Suresh
+      </h1>
+
+      <p className="text-center text-gray-400 mt-4">
+        Frontend / MERN Stack Developer
+      </p>
+    </div>
+  );
+}
+
+export default App;
